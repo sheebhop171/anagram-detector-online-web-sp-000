616a3c5313a0fd86a6b0b25065af597313cc40cd
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-27 21:07:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    list_of_matches = []
+    anagrams.each do |anagram|
+      #binding.pry
+      if anagram == @word
+        list_of_matches << @word
+      else
+      end
+    end
+  end
+end
